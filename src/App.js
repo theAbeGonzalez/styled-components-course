cb48ea768e5dc4ee69fab1ef61c87759dda97113
abrahamgnz <gnz.abr@gmail.com>
2018-02-26 15:03:52 -0600

    Initial example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { Link } from 'react-router-dom'
 import './App.css'
 
+const getLinks = () => [ { label: 'Home', url: '/' }, { label: 'Woof!', url: '/dog' }, { label: 'Hello!', url: '/hello' } ]
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: '/',
+      links: []
+    }
+    this.renderLinks = this.renderLinks.bind(this)
+  }
+
+  componentDidMount () {
+    this.setState({
+      links: getLinks()
+    })
+  }
+
+  renderLinks () {
+    return this.state.links.map(({url, label}) => (
+      <li className='link' key={url}>
+        <Link to={url}>{label}</Link>
+        <span className='border' />
+      </li>
+    ))
+  }
+
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul className='nav'>
+        {this.renderLinks()}
+      </ul>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/hello' component={() => <p>Hello!</p>} />
+      <Route path='/dog' component={() => <p>Woof!</p>} />
+      <Route component={() => <p>404</p>} status={404} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(<Routes />, document.getElementById('root'))
+
 registerServiceWorker()
